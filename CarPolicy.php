862cff0e9369c0ff09967d0e753c51b87008c765
B00162515 Erica Cuthbert <B00162515@mytudublin.ie>
2025-11-12 10:18:14 +0000

    Part !

---
+++ CarPolicy.php
@@ -0,0 +1,32 @@
+<?php
+Class CarPolicy {
+    private $dateOfLastClaim="";
+    private $yearlyPremium=0;
+    private $policyNumber="";
+
+    public function __construct($pn, $yp)
+    {
+        $this->policyNumber=$pn;
+        $this->yearlyPremium=$yp;
+    }
+
+    public function setDateOfLastClaim($dolc)
+    {
+        $this->dateOfLastClaim=$dolc;
+    }
+
+    public function getTotalYearsNoClaims()
+    {
+        $currentDate = new DateTime();
+        $lastDate= new DateTime($this->dateOfLastClaim);
+        $interval = $currentDate->diff($lastDate);
+        return $interval->format("%y");
+    }
+
+    public function __toString()
+    {
+        return "PN: " . $this->policyNumber;
+    }
+}
+
+?>
